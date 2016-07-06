0ef6322ee1947ba08bf2e2e2aaa9dd643de46519
Karthik <joskarthick3@gmail.com>
2016-07-06 12:41:02 +0530

    coraji committted

--- js/app.js
+++ js/app.js
@@ -145,6 +145,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
    
+    .state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login.html'
+      }
+    }
+  })
+ 
 
  ;
   // if none of the above states are matched, use this as the fallback
