d64e73dceac4ac52e519e61c393e73c0b5eef2d0
saranya <saranya7010@gmail.com>
2016-08-11 16:51:26 +0530

    loader

--- js/controllers.js
+++ js/controllers.js
@@ -23,6 +23,9 @@ angular.module('starter.controllers', [ ])
     $ionicLoading.show({
       template: '<p>Loading...</p><ion-spinner></ion-spinner>'
     });
+	 // $timeout(function(){
+        // $scope.show = false;
+    // }, 100);
   };
 
   $scope.hide = function(){
@@ -34,27 +37,22 @@ angular.module('starter.controllers', [ ])
 	$scope.logIn=function(loginData)
 	{
 		
-					
-		 $scope.show();	
+				
+		
 		
 	     $http.post(' http://app.sterlinghsa.com/api/v1/user/login',{username:loginData.username,password:loginData.password},{headers: {'Content-Type':'application/json; charset=utf-8'} })     
 			.success(function(data) {
-				//alert(JSON.stringify(data));
+				$scope.show();		
 				
 				 if(data.status == "SUCCESS"){
-					
+					 
 					localStorage.setItem('access_token',data.access_token);
 					localStorage.setItem('username',loginData.username);
 					//alert(localStorage.getItem('access_token')+"--"+localStorage.getItem('username'));
 					
-					
 					$location.path("/app/portfolio");
-					$scope.hide();
-				
-       
-					
+					$scope.hide();				
 				}else{
-					
 					alert('Enter valid username or password');
 					$scope.hide();
 				}
